5ecce14e65949137e83671b0dc565efa60f4066d
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-01-30 16:03:26 -0800

    Add all valid load instruction operand combinations

--- src/Gameboy.hs
+++ src/Gameboy.hs
@@ -3,6 +3,7 @@ module Gameboy
     ( someFunc
     ) where
 
+import Data.Int
 import Data.Word
 
 data RegType
@@ -33,34 +34,118 @@ data Reg :: RegType -> * where
 
 deriving instance Show (Reg rt)
 
+data OffsetType
+  = RegCOffset
+  | Imm8Offset
+
+data PostOperationType
+  = Increment
+  | Decrement
+
+data StackPointerOperation = None | AddInt8
+
 data OperandKind
   = KImm8
-  | KReg8
+  | KReg8 RegType
   | KImm16
-  | KReg16
-  | KIndirect
+  | KReg16 RegType RegType
+  | KIndirect OperandKind
+  | KFF00Offset OffsetType
+  | KStackPointer StackPointerOperation
+  | KPostInstruction OperandKind PostOperationType
 
 data Addressable = Addresable | NotAddressable
 
 type family Address (ok :: OperandKind) :: Addressable where
   Address 'KImm16 = 'Addresable
-  Address 'KReg16 = 'Addresable
+  Address ('KReg16 _ _) = 'Addresable
   Address _ = 'NotAddressable
+
+data Offsetable = Offsetable OffsetType | NotOffsetable
+
+type family Offset (ok :: OperandKind) :: Offsetable where
+  Offset 'KImm8 = 'Offsetable 'Imm8Offset
+  Offset ('KReg8 'C) = 'Offsetable 'RegCOffset
+  Offset _ = 'NotOffsetable
+
+data StackPointerType :: StackPointerOperation -> * where
+  Default :: StackPointerType 'None
+  AddI8 :: Int8 -> StackPointerType 'AddInt8
+
+deriving instance Show (StackPointerType spo)
+
+data PostInstructionOp :: PostOperationType -> * where
+  Incr :: PostInstructionOp 'Increment
+  Decr :: PostInstructionOp 'Decrement
+
+deriving instance Show (PostInstructionOp ot)
+
+data PostOperable = PostOperable | NotPostOperable
+
+type family PostOperation (ok :: OperandKind) (ot :: PostOperationType) :: PostOperable where
+  PostOperation ('KReg16 'H 'L) 'Increment = 'PostOperable
+  PostOperation ('KReg16 'H 'L) 'Decrement = 'PostOperable
+  PostOperation _ _ = 'NotPostOperable
 
 data Operand :: OperandKind -> * where
   Imm8 :: Word8 -> Operand 'KImm8
-  Reg8 :: Reg r -> Operand 'KReg8
+  Reg8 :: Reg r -> Operand ('KReg8 r)
   Imm16 :: Word16 -> Operand 'KImm16
-  Reg16 :: CombinedRegs r1 r2 ~ 'CombinedRegsValid => Reg r1 -> Reg r2 -> Operand 'KReg16
-  Indirect :: Address ok ~ 'Addresable => Operand ok -> Operand 'KIndirect
+  Reg16 :: CombinedRegs r1 r2 ~ 'CombinedRegsValid => Reg r1 -> Reg r2 -> Operand ('KReg16 r1 r2)
+  Indirect :: Address ok ~ 'Addresable => Operand ok -> Operand ('KIndirect ok)
+  FF00Offset :: Offset ok ~ 'Offsetable ot => Operand ok -> Operand ('KFF00Offset ot)
+  StackPointer :: StackPointerType spo -> Operand ('KStackPointer spo)
+  PostInstruction :: PostOperation ok ot ~ 'PostOperable => Operand ok -> PostInstructionOp ot -> Operand ('KPostInstruction ok ot)
 
 deriving instance Show (Operand ok)
 
 data Loadable = Loadable | NotLoadable
 
 type family LoadOperands (k1 :: OperandKind) (k2 :: OperandKind) :: Loadable where
-  -- TODO: add all valid load operand combinations
-  LoadOperands 'KReg8 'KReg8 = 'Loadable
+  -- LD r8, r8
+  LoadOperands ('KReg8 _) ('KReg8 _) = 'Loadable
+  -- LD r8, n8
+  LoadOperands ('KReg8 _) 'KImm8 = 'Loadable
+  -- LD r16, n16
+  LoadOperands ('KReg16 _ _) 'KImm16 = 'Loadable
+  -- LD (HL), r8
+  LoadOperands ('KIndirect ('KReg16 'H 'L)) ('KReg8 _) = 'Loadable
+  -- LD (HL), n8
+  LoadOperands ('KIndirect ('KReg16 'H 'L)) 'KImm8 = 'Loadable
+  -- LD r8, (HL)
+  LoadOperands ('KReg8 _) ('KIndirect ('KReg16 'H 'L)) = 'Loadable
+  -- LD (r16), A
+  LoadOperands ('KIndirect ('KReg16 _ _)) ('KReg8 'A) = 'Loadable
+  -- LD (n16), A
+  LoadOperands ('KIndirect 'KImm16) ('KReg8 'A) = 'Loadable
+  -- LD ($FF00 + n8), A
+  LoadOperands ('KFF00Offset 'Imm8Offset) ('KReg8 'A) = 'Loadable
+  -- LD ($FF00 + C), A
+  LoadOperands ('KFF00Offset 'RegCOffset) ('KReg8 'A) = 'Loadable
+  -- LD A, (r16)
+  LoadOperands ('KReg8 'A) ('KIndirect ('KReg16 _ _)) = 'Loadable
+  -- LD A, (n16)
+  LoadOperands ('KReg8 'A) ('KIndirect 'KImm16) = 'Loadable
+  -- LD A, ($FF00 + n8)
+  LoadOperands ('KReg8 'A) ('KFF00Offset 'Imm8Offset) = 'Loadable
+  -- LD A, ($FF00 + C)
+  LoadOperands ('KReg8 'A) ('KFF00Offset 'RegCOffset) = 'Loadable
+  -- LD (HLI), A
+  LoadOperands ('KIndirect ('KPostInstruction ('KReg16 'H 'L) 'Increment)) ('KReg8 'A) = 'Loadable
+  -- LD (HLD), A
+  LoadOperands ('KIndirect ('KPostInstruction ('KReg16 'H 'L) 'Decrement)) ('KReg8 'A) = 'Loadable
+  -- LD A, (HLI)
+  LoadOperands ('KReg8 'A) ('KIndirect ('KPostInstruction ('KReg16 'H 'L) 'Increment)) = 'Loadable
+  -- LD A, (HLD)
+  LoadOperands ('KReg8 'A) ('KIndirect ('KPostInstruction ('KReg16 'H 'L) 'Decrement)) = 'Loadable
+  -- LD SP, n16
+  LoadOperands ('KStackPointer 'None) 'KImm16 = 'Loadable
+  -- LD (n16), SP
+  LoadOperands ('KIndirect 'KImm16) ('KStackPointer 'None) = 'Loadable
+  -- LD HL, SP + e8
+  LoadOperands ('KReg16 'H 'L) ('KStackPointer 'AddInt8) = 'Loadable
+  -- LD SP, HL
+  LoadOperands ('KStackPointer 'None) ('KReg16 'H 'L) = 'Loadable
   LoadOperands _ _ = 'NotLoadable
 
 data InstructionKind

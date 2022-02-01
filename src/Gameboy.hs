977e8ab2c70250bf834938f6184b2ae3278ddc34
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-02-01 15:53:26 -0800

    Add convenience IndirectHL(Kind), Sub/Or instructions, generalize operands type families

--- src/Gameboy.hs
+++ src/Gameboy.hs
@@ -51,6 +51,8 @@ data OperandKind
   | KStackPointer StackPointerOperationKind
   | KPostInstruction OperandKind PostInstructionOperationKind
 
+type IndirectHLKind = 'KIndirect ('KReg16 'H 'L)
+
 data Addressable = Addresable | NotAddressable
 
 type family Address (ok :: OperandKind) :: Addressable where
@@ -91,102 +93,12 @@ data Operand :: OperandKind -> * where
   Uimm16 :: Word16 -> Operand 'KUimm16
   Reg16 :: CombinedRegs r1 r2 ~ 'CombinedRegsValid => Reg r1 -> Reg r2 -> Operand ('KReg16 r1 r2)
   Indirect :: Address ok ~ 'Addresable => Operand ok -> Operand ('KIndirect ok)
+  IndirectHL :: Operand IndirectHLKind
   FF00Offset :: Offset ok ~ 'Offsetable ot => Operand ok -> Operand ('KFF00Offset ot)
   StackPointer :: StackPointerOperation k -> Operand ('KStackPointer k)
   PostInstruction :: PostOperation ok piok ~ 'PostOperable => Operand ok -> PostInstructionOperation piok -> Operand ('KPostInstruction ok ot)
 
 deriving instance Show (Operand ok)
-
-data Loadable = Loadable | NotLoadable
-
-type family LoadOperands (k1 :: OperandKind) (k2 :: OperandKind) :: Loadable where
-  -- LD r8, r8
-  LoadOperands ('KReg8 _) ('KReg8 _) = 'Loadable
-  -- LD r8, n8
-  LoadOperands ('KReg8 _) 'KUimm8 = 'Loadable
-  -- LD r16, n16
-  LoadOperands ('KReg16 _ _) 'KUimm16 = 'Loadable
-  -- LD (HL), r8
-  LoadOperands ('KIndirect ('KReg16 'H 'L)) ('KReg8 _) = 'Loadable
-  -- LD (HL), n8
-  LoadOperands ('KIndirect ('KReg16 'H 'L)) 'KUimm8 = 'Loadable
-  -- LD r8, (HL)
-  LoadOperands ('KReg8 _) ('KIndirect ('KReg16 'H 'L)) = 'Loadable
-  -- LD (r16), A
-  LoadOperands ('KIndirect ('KReg16 _ _)) ('KReg8 'A) = 'Loadable
-  -- LD (n16), A
-  LoadOperands ('KIndirect 'KUimm16) ('KReg8 'A) = 'Loadable
-  -- LD ($FF00 + n8), A | LD ($FF00 + C), A
-  LoadOperands ('KFF00Offset _) ('KReg8 'A) = 'Loadable
-  -- LD A, (r16)
-  LoadOperands ('KReg8 'A) ('KIndirect ('KReg16 _ _)) = 'Loadable
-  -- LD A, (n16)
-  LoadOperands ('KReg8 'A) ('KIndirect 'KUimm16) = 'Loadable
-  -- LD A, ($FF00 + n8) | LD A, ($FF00 + C)
-  LoadOperands ('KReg8 'A) ('KFF00Offset _) = 'Loadable
-  -- LD (HLI), A | LD (HLD), A
-  LoadOperands ('KIndirect ('KPostInstruction ('KReg16 'H 'L) _)) ('KReg8 'A) = 'Loadable
-  -- LD A, (HLI) | LD A, (HLD)
-  LoadOperands ('KReg8 'A) ('KIndirect ('KPostInstruction ('KReg16 'H 'L) _)) = 'Loadable
-  -- LD SP, n16
-  LoadOperands ('KStackPointer 'KUnchanged) 'KUimm16 = 'Loadable
-  -- LD (n16), SP
-  LoadOperands ('KIndirect 'KUimm16) ('KStackPointer 'KUnchanged) = 'Loadable
-  -- LD HL, SP + e8
-  LoadOperands ('KReg16 'H 'L) ('KStackPointer 'KAddInt8) = 'Loadable
-  -- LD SP, HL
-  LoadOperands ('KStackPointer 'KUnchanged) ('KReg16 'H 'L) = 'Loadable
-  LoadOperands _ _ = 'NotLoadable
-
-data AddTypeKind = KWithCarryAdded | KWithoutCarryAdded
-
-data AddType :: AddTypeKind -> * where
-  WithCarryAdded :: AddType 'KWithCarryAdded
-  WithoutCarryAdded :: AddType 'KWithoutCarryAdded
-
-data Addable = Addable | NotAddable
-
-type family AddOperands (atk :: AddTypeKind) (k1 :: OperandKind) (k2 :: OperandKind) :: Addable where
-  AddOperands _ ('KReg8 'A) ('KReg8 _) = 'Addable
-  AddOperands _ ('KReg8 'A) ('KIndirect ('KReg16 'H 'L)) = 'Addable
-  AddOperands _ ('KReg8 'A) 'KUimm8 = 'Addable
-  AddOperands 'KWithoutCarryAdded ('KReg16 'H 'L) ('KStackPointer 'KUnchanged) = 'Addable
-  AddOperands 'KWithoutCarryAdded ('KStackPointer 'KUnchanged) 'KImm8 = 'Addable
-  AddOperands _ _ _ = 'NotAddable
-
-data Andable = Andable | NotAndable
-
-type family AndOperands (k1 :: OperandKind) (k2 :: OperandKind) :: Andable where
-  AndOperands ('KReg8 'A) ('KReg8 _) = 'Andable
-  AndOperands ('KReg8 'A) ('KIndirect ('KReg16 'H 'L)) = 'Andable
-  AndOperands ('KReg8 'A) 'KUimm8 = 'Andable
-  AndOperands _ _ = 'NotAndable
-
-data Comparable = Comparable | NotComparable
-
-type family CompareOperands (k1 :: OperandKind) (k2 :: OperandKind) :: Comparable where
-  CompareOperands ('KReg8 'A) ('KReg8 _) = 'Comparable
-  CompareOperands ('KReg8 'A) ('KIndirect ('KReg16 'H 'L)) = 'Comparable
-  CompareOperands ('KReg8 'A) 'KUimm8 = 'Comparable
-  CompareOperands _ _ = 'NotComparable
-
-data Decrementable = Decrementable | NotDecrementable
-
-type family DecrementOperand (ok :: OperandKind) :: Decrementable where
-  DecrementOperand ('KReg8 _) = 'Decrementable
-  DecrementOperand ('KIndirect ('KReg16 'H 'L)) = 'Decrementable
-  DecrementOperand ('KReg16 _ _) = 'Decrementable
-  DecrementOperand ('KStackPointer 'KUnchanged) = 'Decrementable
-  DecrementOperand _ = 'NotDecrementable
-
-data Incrementable = Incrementable | NotIncrementable
-
-type family IncrementOperand (ok :: OperandKind) :: Incrementable where
-  IncrementOperand ('KReg8 _) = 'Incrementable
-  IncrementOperand ('KIndirect ('KReg16 'H 'L)) = 'Incrementable
-  IncrementOperand ('KReg16 _ _) = 'Incrementable
-  IncrementOperand ('KStackPointer 'KUnchanged) = 'Incrementable
-  IncrementOperand _ = 'NotIncrementable
 
 data InstructionKind
   = KLoad
@@ -195,35 +107,138 @@ data InstructionKind
   | KCompare
   | KDecrement
   | KIncrement
+  | KOr
+  | KSub
+  | KInvalid
+
+type family LoadOperands (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  -- LD r8, r8
+  LoadOperands ('KReg8 _) ('KReg8 _) = 'KLoad
+  -- LD r8, n8
+  LoadOperands ('KReg8 _) 'KUimm8 = 'KLoad
+  -- LD r16, n16
+  LoadOperands ('KReg16 _ _) 'KUimm16 = 'KLoad
+  -- LD (HL), r8
+  LoadOperands IndirectHLKind ('KReg8 _) = 'KLoad
+  -- LD (HL), n8
+  LoadOperands IndirectHLKind 'KUimm8 = 'KLoad
+  -- LD r8, (HL)
+  LoadOperands ('KReg8 _) IndirectHLKind = 'KLoad
+  -- LD (r16), A
+  LoadOperands ('KIndirect ('KReg16 _ _)) ('KReg8 'A) = 'KLoad
+  -- LD (n16), A
+  LoadOperands ('KIndirect 'KUimm16) ('KReg8 'A) = 'KLoad
+  -- LD ($FF00 + n8), A | LD ($FF00 + C), A
+  LoadOperands ('KFF00Offset _) ('KReg8 'A) = 'KLoad
+  -- LD A, (r16)
+  LoadOperands ('KReg8 'A) ('KIndirect ('KReg16 _ _)) = 'KLoad
+  -- LD A, (n16)
+  LoadOperands ('KReg8 'A) ('KIndirect 'KUimm16) = 'KLoad
+  -- LD A, ($FF00 + n8) | LD A, ($FF00 + C)
+  LoadOperands ('KReg8 'A) ('KFF00Offset _) = 'KLoad
+  -- LD (HLI), A | LD (HLD), A
+  LoadOperands ('KIndirect ('KPostInstruction ('KReg16 'H 'L) _)) ('KReg8 'A) = 'KLoad
+  -- LD A, (HLI) | LD A, (HLD)
+  LoadOperands ('KReg8 'A) ('KIndirect ('KPostInstruction ('KReg16 'H 'L) _)) = 'KLoad
+  -- LD SP, n16
+  LoadOperands ('KStackPointer 'KUnchanged) 'KUimm16 = 'KLoad
+  -- LD (n16), SP
+  LoadOperands ('KIndirect 'KUimm16) ('KStackPointer 'KUnchanged) = 'KLoad
+  -- LD HL, SP + e8
+  LoadOperands ('KReg16 'H 'L) ('KStackPointer 'KAddInt8) = 'KLoad
+  -- LD SP, HL
+  LoadOperands ('KStackPointer 'KUnchanged) ('KReg16 'H 'L) = 'KLoad
+  LoadOperands _ _ = 'KInvalid
+
+data ArithmeticTypeKind = KWithCarryIncluded | KWithoutCarryIncluded
+
+data ArithmeticType :: ArithmeticTypeKind -> * where
+  WithCarryIncluded :: ArithmeticType 'KWithCarryIncluded
+  WithoutCarryIncluded :: ArithmeticType 'KWithoutCarryIncluded
+
+type family AddOperands (atk :: ArithmeticTypeKind) (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  AddOperands _ ('KReg8 'A) ('KReg8 _) = 'KAdd
+  AddOperands _ ('KReg8 'A) IndirectHLKind = 'KAdd
+  AddOperands _ ('KReg8 'A) 'KUimm8 = 'KAdd
+  AddOperands 'KWithoutCarryIncluded ('KReg16 'H 'L) ('KStackPointer 'KUnchanged) = 'KAdd
+  AddOperands 'KWithoutCarryIncluded ('KStackPointer 'KUnchanged) 'KImm8 = 'KAdd
+  AddOperands _ _ _ = 'KInvalid
+
+type family AndOperands (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  AndOperands ('KReg8 'A) ('KReg8 _) = 'KAnd
+  AndOperands ('KReg8 'A) IndirectHLKind = 'KAnd
+  AndOperands ('KReg8 'A) 'KUimm8 = 'KAnd
+  AndOperands _ _ = 'KInvalid
+
+type family CompareOperands (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  CompareOperands ('KReg8 'A) ('KReg8 _) = 'KCompare
+  CompareOperands ('KReg8 'A) IndirectHLKind = 'KCompare
+  CompareOperands ('KReg8 'A) 'KUimm8 = 'KCompare
+  CompareOperands _ _ = 'KInvalid
+
+type family DecrementOperand (ok :: OperandKind) :: InstructionKind where
+  DecrementOperand ('KReg8 _) = 'KDecrement
+  DecrementOperand IndirectHLKind = 'KDecrement
+  DecrementOperand ('KReg16 _ _) = 'KDecrement
+  DecrementOperand ('KStackPointer 'KUnchanged) = 'KDecrement
+  DecrementOperand _ = 'KInvalid
+
+type family IncrementOperand (ok :: OperandKind) :: InstructionKind where
+  IncrementOperand ('KReg8 _) = 'KIncrement
+  IncrementOperand IndirectHLKind = 'KIncrement
+  IncrementOperand ('KReg16 _ _) = 'KIncrement
+  IncrementOperand ('KStackPointer 'KUnchanged) = 'KIncrement
+  IncrementOperand _ = 'KInvalid
+
+type family OrOperands (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  OrOperands ('KReg8 'A) ('KReg8 _) = 'KOr
+  OrOperands ('KReg8 'A) IndirectHLKind = 'KOr
+  OrOperands ('KReg8 'A) 'KUimm8 = 'KOr
+  OrOperands _ _ = 'KInvalid
+
+type family SubOperands (k1 :: OperandKind) (k2 :: OperandKind) :: InstructionKind where
+  SubOperands ('KReg8 'A) ('KReg8 _) = 'KSub
+  SubOperands ('KReg8 'A) IndirectHLKind = 'KSub
+  SubOperands ('KReg8 'A) 'KUimm8 = 'KSub
+  SubOperands _ _ = 'KInvalid
 
 data Instruction :: InstructionKind -> * where
-  Load :: LoadOperands k1 k2 ~ 'Loadable => Operand k1 -> Operand k2 -> Instruction 'KLoad
-  Add :: AddOperands atk k1 k2 ~ 'Addable => AddType atk -> Operand k1 -> Operand k2 -> Instruction 'KAdd
-  And :: AndOperands k1 k2 ~ 'Andable => Operand k1 -> Operand k2 -> Instruction 'KAnd
-  Compare :: CompareOperands k1 k2 ~ 'Comparable => Operand k1 -> Operand k2 -> Instruction 'KCompare
-  Decrement :: DecrementOperand ok ~ 'Decrementable => Operand ok -> Instruction 'KDecrement
-  Increment :: IncrementOperand ok ~ 'Incrementable => Operand ok -> Instruction 'KIncrement
+  Load :: LoadOperands k1 k2 ~ 'KLoad => Operand k1 -> Operand k2 -> Instruction 'KLoad
+  Add :: AddOperands atk k1 k2 ~ 'KAdd => ArithmeticType atk -> Operand k1 -> Operand k2 -> Instruction 'KAdd
+  And :: AndOperands k1 k2 ~ 'KAnd => Operand k1 -> Operand k2 -> Instruction 'KAnd
+  Compare :: CompareOperands k1 k2 ~ 'KCompare => Operand k1 -> Operand k2 -> Instruction 'KCompare
+  Decrement :: DecrementOperand ok ~ 'KDecrement => Operand ok -> Instruction 'KDecrement
+  Increment :: IncrementOperand ok ~ 'KIncrement => Operand ok -> Instruction 'KIncrement
+  Or :: OrOperands k1 k2 ~ 'KOr => Operand k1 -> Operand k2 -> Instruction 'KOr
+  Sub :: SubOperands k1 k2 ~ 'KSub => ArithmeticType atk -> Operand k1 -> Operand k2 -> Instruction 'KSub
 
+-- TODO: implement
 executeInstruction :: Instruction k -> IO ()
 executeInstruction ins@(Load _ _) = loadIns ins where
   loadIns :: Instruction 'KLoad -> IO ()
-  loadIns _ = undefined -- TODO: implement
+  loadIns _ = undefined
 executeInstruction ins@(Add _ _ _) = addIns ins where
   addIns :: Instruction 'KAdd -> IO ()
-  addIns (Add WithCarryAdded _ _) = undefined -- TODO: implement
-  addIns (Add WithoutCarryAdded _ _) = undefined -- TODO: implement
+  addIns (Add WithCarryIncluded _ _) = undefined
+  addIns (Add WithoutCarryIncluded _ _) = undefined
 executeInstruction ins@(And _ _) = andIns ins where
   andIns :: Instruction 'KAnd -> IO ()
-  andIns _ = undefined -- TODO: implement
+  andIns _ = undefined
 executeInstruction ins@(Compare _ _) = compareIns ins where
   compareIns :: Instruction 'KCompare -> IO ()
-  compareIns _ = undefined -- TODO: implement
+  compareIns _ = undefined
 executeInstruction ins@(Decrement _) = decrementIns ins where
   decrementIns :: Instruction 'KDecrement -> IO ()
-  decrementIns = undefined -- TODO: implement
+  decrementIns = undefined
 executeInstruction ins@(Increment _) = incrementIns ins where
   incrementIns :: Instruction 'KIncrement -> IO ()
-  incrementIns = undefined -- TODO: implement
+  incrementIns _ = undefined
+executeInstruction ins@(Or _ _) = orIns ins where
+  orIns :: Instruction 'KOr -> IO ()
+  orIns = undefined
+executeInstruction ins@(Sub _ _ _) = subIns ins where
+  subIns :: Instruction 'KSub -> IO ()
+  subIns = undefined
 
 someFunc :: IO ()
 someFunc = do
